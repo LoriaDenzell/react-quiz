06d6ec1cc565ec301ee3a67a93e22bf8bab223c3
LoriaDenzell <loriadenzell@gmail.com>
2021-02-22 23:57:31 -0800

    First commit for this react project

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "Which city is known as the “Walled City?”",
+    answers: ["Malolos", "Intramuros", "Makati", "Calamba"],
+    correct: "Intramuros",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Which country occupied the Philippines during World War II?',
+    answers: ["Germany", "United States of America", "Japan", "Norway"],
+    correct: "Japan",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Philippines Independence Day is celebrated on what date?",
+    answers: ["June 12", "September 18", "July 4", "May 1"],
+    correct: "June 12",
+    questionId: "267908"
+  },
+  {
+    question: "The Bataan Death March took place in what year?",
+    answers: [
+      "1890",
+      "1912",
+      "1942",
+      "1932"
+    ],
+    correct: "1942",
+    questionId: "333247"
+  },
+  {
+    question: "Who was known as the Hero of Tirad Pass?",
+    answers: ["Gregorio Del Pilar", "Andress Bonifacio", "Dr. Jose Rizal", "Emilio Aguinaldo"],
+    correct: "Gregorio Del Pilar",
+    questionId: "496293"
+  },
+  {
+    question:
+      "In what year did the Portuguese explorer Ferdinand Magellan arrive in the shores of Philippines?",
+    answers: [
+      "1519",
+      "1520",
+      "1521",
+      "1522"
+    ],
+    correct: "1521",
+    questionId: "588909"
+  },
+  {
+    question:
+      'Mayon Volcano is located in which province?',
+    answers: ["Sorsogon", "Zambales", "Batangas", "Albay"],
+    correct: "Albay",
+    questionId: "648452"
+  },
+  {
+    question: "Gloria Diaz won the Miss Universe contest in what year?",
+    answers: ["1959", "1969", "1979", "1989"],
+    correct: "1969",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Who said this immortal words “A Filipino is worth dying for?” ",
+    answers: ["Ninoy Aquino", "Ferdinand Marcos", "Diosdado Macapagal", "Emilio Jacinto"],
+    correct: "Ninoy Aquino",
+    questionId: "839754"
+  },
+  {
+    question:
+      "What is a fertilized duck egg called?",
+    answers: [
+      "Balut",
+      "Tukneneng",
+      "Itlog",
+      "Scrambled Egg"
+    ],
+    correct: "Balut",
+    questionId: "98390"
+  },
+  {
+    question: "What is the original name of Luneta park?",
+    answers: ["Rizal Park", "Pook Pasyalan", "Manila Park", "Bagumbayan"],
+    correct: "Bagumbayan",
+    questionId: "1071006"
+  },
+  {
+    question: "What are the provinces that consist of the acronym CALABARZON?",
+    answers: ["Cagayan, Landusan, Baguio, Romblon, Quinaguitman", "Cavite, Laguna, Batangas, Rizal, Quezon", "Capiz, Langob, Bohol, Rosario, Quirhat", "Camarines Sur, La Union, Bukidnon, Romblon, Quirhat"],
+    correct: "Cavite, Laguna, Batangas, Rizal, Quezon",
+    questionId: "1174154"
+  },
+  {
+    question: "Mt Pinatubo is located in the province of Luzon at the intersection of the borders of the provinces of Zambales, Tarlac, and _________",
+    answers: ["Pampanga", "Lapaz", "Nueva Ecija", "Laguna"],
+    correct: "Pampanga",
+    questionId: "1226535"
+  },
+  {
+    question: "“Pahiyas” is a festival celebrated every May in which town in Quezon province?",
+    answers: ["Atimonan", "Lucena", "Lucban", "Tagkawayan"],
+    correct: "Lucban",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the residence of the President of the Philippines popularly called?",
+    answers: ["Presidential Palace", "Malacañang Palace", "The Mansion", "White House"],
+    correct: "Malacañang Palace",
+    questionId: "1436873"
+  },
+  {
+    question: "Who is the youngest general in Emilio Aguinaldo's Army?",
+    answers: ["Gregorio Del Pilar", "Mariano Alvarez", "Manuel Tinio", "Antonio Luna"],
+    correct: "Manuel Tinio",
+    questionId: "1515110"
+  },
+  {
+    question: "This Filipino president topped the 1939 Bar Examinations (with near-perfect score) despite being in jail for 27 days. Who is he?",
+    answers: ["Carlos Garcia", "Ferdinand Marcos", "Rodrigo Duterte", "Diosdado Macapagal"],
+    correct: "Ferdinand Marcos",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "What is the LRT 2 station whose name is the Spanish word for purity?",
+    answers: ["Santolan", "Anonas", "Gilmore", "Pureza"],
+    correct: "Pureza",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "What is the Largest Island in the Philippines?",
+    answers: ["Mindanao", "Negros", "Samar", "Luzon"],
+    correct: "Luzon",
+    questionId: "1822532"
+  },
+  {
+    question: "Slang term for a person from the Philippines?",
+    answers: [
+      "Pinoy",
+      "Filipino",
+      "Pilipino",
+      "None of the choices"
+    ],
+    correct: "Pinoy",
+    questionId: "195075"
+  },
+  {
+    question: "what is the first name of EDSA (road)?",
+    answers: ["A dance", "A medical procedure", "A sport", "A language"],
+    correct: "A dance",
+    questionId: "2019778"
+  },
+  {
+    question: "What is the capital of the Philippines?",
+    answers: ["North-south Circumferential road", "Highway 54", "Avenida 19 de junio", "Manila Diversion Road"],
+    correct: "Manila",
+    questionId: "2134343"
+  },
+  {
+    question: "what is the most populous city of the Philippines?",
+    answers: [
+      "Quezon City",
+      "Manila",
+      "Baguio City",
+      "Makati City"
+    ],
+    correct: "Quezon City",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "What is the southernmost island of the Philippines archipelago?",
+    answers: ["Tawitawi Island", "Sulu", "Saluag", "Sitangkai"],
+    correct: "Saluag",
+    questionId: "2379831"
+  },
+  {
+    question: "What is the northernmost island of the Philippines archipelago?",
+    answers: ["Siayan Island", "Mavudis Island", "Mavulis Island", "Itbayat Island"],
+    correct: "Mavulis Island",
+    questionId: "2426418"
+  },
+  {
+    question: "What is the first expressway in the Philippines?",
+    answers: ["North Luzon Expressway", "South Luzon Expressway", "Manila-Cavite Expressway", "Southern Tagalog Arterial Road"],
+    correct: "North Luzon Expressway",
+    questionId: "2510086"
+  },
+  {
+    question: "What continent is the Philippines in?",
+    answers: [
+      "Australia",
+      "Europe",
+      "South America",
+      "Asia"
+    ],
+    correct: "Asia",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "What religion is most prominent in the Philippines?",
+    answers: ["Christianity", "Muslim", "Roman Catholic", "Buddhism"],
+    correct: "Roman Catholic",
+    questionId: "2796884"
+  },
+  {
+    question: "How many provinces are in the Philippines?",
+    answers: ["80", "81", "70", "71"],
+    correct: "81",
+    questionId: "2838900"
+  },
+  {
+    question: 'What is the oldest city of the Philippines?',
+    answers: ["Manila", "Cebu", "Mactan", "Magallanes"],
+    correct: "Cebu",
+    questionId: "298060"
+  },
+  {
+    question: "What is the tallest mountain in the Philippines?",
+    answers: ["Mt. Apo", "Mt. Kanlaon", "Mt. Pinatubo", "Mt. Mayon"],
+    correct: "Mt. Apo",
+    questionId: "3096579"
+  },
+  {
+    question: "Who is depicted on the 100 pesos bill?",
+    answers: [
+      "Sergio Osmeña",
+      "Jose Rizal",
+      "Manuel Quezon",
+      "Manuel Roxas"
+    ],
+    correct: "Manuel Roxas",
+    questionId: "3182461"
+  },
+  {
+    question: "What province does Taal Volcano is located?",
+    answers: [
+      "Cavite",
+      "Laguna",
+      "Quezon Province",
+      "Batangas"
+    ],
+    correct: "Batangas",
+    questionId: "3239112"
+  },
+  {
+    question: "When does the Martial Law was declared by Ferdinand Marcos?",
+    answers: ["September 20, 1972", "September 21, 1972", "September 22, 1971", "September 23, 1971"],
+    correct: "September 21, 1972",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'What is the longest river in the Philippines?',
+    answers: ["Pasig River", "Cagayan River", "Bicol River", "Agusan River"],
+    correct: "Cagayan River",
+    questionId: "3410327"
+  },
+  {
+    question: "What is the first name of Philippine National Railways (PNR)?",
+    answers: ["Philippine Railroad Company", "Manila Railroad Company", "Ferrocaril de Manila-Dagupan", "Ferrocaril de Manila-Legazpi"],
+    correct: "Ferrocaril de Manila-Dagupan",
+    questionId: "3542596"
+  },
+  {
+    question: "What is the largest province in the Philippines in terms of land area?",
+    answers: ["Isabela", "Palawan", "Cebu", "Bukidnon"],
+    correct: "Palawan",
+    questionId: "3667517"
+  },
+  {
+    question: "The Philippines is composed of approximately how many islands?",
+    answers: [
+      "7,100",
+      "7,641",
+      "7,500",
+      "7,642"
+    ],
+    correct: "7,641",
+    questionId: "3791672"
+  },
+  {
+    question: "Who named the Philippines?",
+    answers: ["Ruy Lopez de Villlalobos", "King Philip II", "Magellan", "Miguel López de Legazpi"],
+    correct: "Ruy Lopez de Villlalobos",
+    questionId: "3893585"
+  },
+  {
+    question: 'Who is the founder of Katastaasang Kagalanggalangang Katipuanan ng mga Anak ng Bayan (KKK)?',
+    answers: ["Antonio Luna", "Andres Bonifacio", "Emilio Aguinaldo", "Gregorio del Pilar"],
+    correct: "Andres Bonifacio",
+    questionId: "3913430"
+  },
+  {
+    question: "What is considered the earliest form of writing in the Philippines?",
+    answers: ["indus script", "baybayin", "cuneiform", "none of the choices"],
+    correct: "baybayin",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Who led the longest revolt in the Philippines during the Spanish times?",
+    answers: ["Emilio Jacinto", "Andres Bonifacio", "Francisco Dagohoy", "Jose Rizal"],
+    correct: "Francisco Dagohoy",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "Diariong Tagalog was founded by:",
+    answers: ["Pedro Laktaw", "Juan Luna", "Marcelo del Pilar", "Gregorio Sanciano"],
+    correct: "Marcelo del Pilar",
+    questionId: "4235063"
+  },
+  {
+    question: "The Cry of Pugad Lawin, marked the start of the: ",
+    answers: ["Philippine Revolution", "Bonifacio's leadership in the KKK", "KKK as a secret society", "Formation of the Philippine Government"],
+    correct: "Philippine Revolution",
+    questionId: "4321002"
+  },
+  {
+    question: "He arrived in Cebu in 1565 to establish the first Spanish settlement in the Philippines, marking the beginning of Spain's colonization and Christianization of the archipelago. Who was he?",
+    answers: [
+      "Ferdinand Magellan",
+      "Garcia Jofre Loaisa",
+      "Sebastian Cabot",
+      "Miguel Lopez de Legazpi"
+    ],
+    correct: "Miguel Lopez de Legazpi",
+    questionId: "4442286"
+  },
+  {
+    question: "What is the largest lake in the Philippines?",
+    answers: [
+      "Angat Lake",
+      "Laguna Lake",
+      "Taal Lake",
+      "Pantabangan Lake"
+    ],
+    correct: "Laguna Lake",
+    questionId: "4564430"
+  },
+  {
+    question: "Internet in the Philippines first became avaibale on:",
+    answers: ["March 29, 1994", "March 29, 1993", "March 29, 1992", "March 29, 1991"],
+    correct: "March 29, 1994",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "What is the deadliest typhoon that struck the Philippines?",
+    answers: ["Haiphong", "Haiyan", "Uring", "Angela"],
+    correct: "Haiphong",
+    questionId: "4795960"
+  },
+  {
+    question: "What is the present name of the province in mainland Luzon where the first mass occured?",
+    answers: ["Albay", "Sorsogon", "Camarines Sur", "Manila"],
+    correct: "Sorsogon",
+    questionId: "4811162"
+  },
+  {
+    question: "What is the total length of Pasig River?",
+    answers: ["25 KM", "26 KM", "27 KM", "28 KM"],
+    correct: "25 KM",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
